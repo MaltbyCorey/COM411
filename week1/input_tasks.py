3be8bf3d5d7211f736ff5a343f74117d1178f007
Corey Maltby <corey.maltby@yahoo.co.uk>
2023-09-29 10:41:58 +0100

    Added code to display an ASCII robot with dynamic eyes.

--- week1/input_tasks.py
+++ week1/input_tasks.py
@@ -2,3 +2,9 @@
 print("What is your name?")
 name = input()
 print(f"It is nice to meet you {name}")
+
+# Display a box
+print("##########")
+print("# 0    0 #")
+print("#   __   #")
+print("##########")
