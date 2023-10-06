7b38fc3d47426f166381b63348e07a37e4591346
Corey Maltby <corey.maltby@yahoo.co.uk>
2023-10-06 11:33:03 +0100

    Added code to demonstrate the use of a modulus operator.

---
+++ week 2/Modulo Operator.py
@@ -0,0 +1,12 @@
+"""
+# Asks user for a whole number
+print("Please enter a whole number")
+number = int(input())
+
+# Divides number by 2, if the remainder is 0 the number is even
+if number % 2 == 0:
+    print(f"The number {number} is an even number")
+else:
+    print(f"The number {number} is an odd number")
+"""
+
