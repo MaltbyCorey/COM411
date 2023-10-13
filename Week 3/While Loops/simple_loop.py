54cb053434056872b882651829b9ec96bde8fa51
Corey Maltby <corey.maltby@yahoo.co.uk>
2023-10-13 11:06:22 +0100

    Added code to demonstrate the use of a simple while loop.

---
+++ Week 3/While Loops/simple_loop.py
@@ -0,0 +1,11 @@
+# Gets the amount of apples to remove
+print("How many apples should I remove?")
+amount_to_remove = int(input())
+apples_removed = 0
+
+# Loop until the right amount of apples removed
+while apples_removed != amount_to_remove:
+    print("Removed apple.")
+    # Increments counter
+    apples_removed += 1
+
