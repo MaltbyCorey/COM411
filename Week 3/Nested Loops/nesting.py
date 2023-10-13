63d9c1da18644e0298ca325686574ff88bfa9b6c
Corey Maltby <corey.maltby@yahoo.co.uk>
2023-10-13 12:40:29 +0100

    Added alternatice method

--- Week 3/Nested Loops/nesting.py
+++ Week 3/Nested Loops/nesting.py
@@ -7,7 +7,7 @@ print("Please enter the character for the marker")
 marker = input()
 marker1 = -1
 marker2 = -1
-
+"""
 # Loops for the amount of characters in string
 for position in range(0, len(sequence), 1):
     letter = sequence[position]
@@ -24,3 +24,40 @@ for position in range(0, len(sequence), 1):
 
 # Output the distance to user
 print(f"The distance between the marker is {marker2 - marker1 -1}")
+
+
+for i, letter in enumerate(sequence):
+
+    # If the letter is equal to marker
+    if letter == marker:
+        # If marker 1 is -1
+        if marker1 == -1:
+            # Add the position to marker1
+            marker1 = i
+        # Else add the position to marker2x---x
+        else:
+            marker2 = i
+
+# Output the distance to user
+print(f"The distance between the marker is {marker2 - marker1 -1}")
+"""
+x = False
+count = 0
+for position in range(0, len(sequence), 1):
+    letter = sequence[position]
+
+    # If the letter is the marker
+    if letter == marker:
+        # Toggles x
+        if x:
+            x = False
+        else:
+            x = True
+
+    # If x is true
+    if x:
+        # If letter is -
+        if letter == "-":
+            count += 1
+
+print(f"The distance between the marker is {count}")
