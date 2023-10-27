f5a5898fc5b7fc17bb5b3d8bbd030540f4acdd37
Corey Maltby <corey.maltby@yahoo.co.uk>
2023-10-27 11:34:37 +0100

    Added code to demonstrate how to iterate a list.

--- week5/Lists/iterate.py
+++ week5/Lists/iterate.py
@@ -1,1 +1,20 @@
-import week5.indexing as
+import week5.lists.indexing as indexing
+
+
+def menu():
+    print("Please select a direction")
+    movements = indexing.movements()
+
+    # Prints out all the directions
+    num = 0
+    for direct in [0, 2, 4, 6]:
+        print(f"{num}: {movements[direct]}")
+        num += 1
+
+
+def run_task3():
+    menu()
+
+
+if __name__ == "__main__":
+    run_task3()
