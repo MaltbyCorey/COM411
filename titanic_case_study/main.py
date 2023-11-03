3b8b507e690582c3917414ac996eee3cb4b9f870
Corey Maltby <corey.maltby@yahoo.co.uk>
2023-11-03 12:26:33 +0000

    Added code to display a menu of options.

--- titanic_case_study/main.py
+++ titanic_case_study/main.py
@@ -20,11 +20,35 @@ def local_data(file_path):
     print("Done.")
 
 
+# Displays the menu to the user
+def display_menu():
+    print(
+        """
+        Please select one of the following options:
+        [1] Display the names of all passengers
+        [2] Display the number of passengers that survived
+        [3] Display the number of passengers per gender
+        [4] Display the number of passengers per age group
+        [5] Display the number of survivors per age group"
+        """)
+
+    # Returns the users input
+    return int(input())
+
+
+# Runs the program
 def run():
+    # Loads the dataset
     local_data('titanic.csv')
+    # Gets the total amount of records loaded
     num_records = len(records)
+    # Displays the total records to the user
     print(f"Successfully loaded {num_records} records")
+
+    # Displays the menu and gets the option chose
+    selected_option = display_menu()
+    print(f"You have selected option {selected_option}")
 
 
 if __name__ == "__main__":
-  run()
+    run()
