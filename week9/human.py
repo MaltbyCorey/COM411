015f32bf62cc85a9880c5c24d756a6c211b6d172
Corey Maltby <corey.maltby@yahoo.co.uk>
2023-11-24 11:40:30 +0000

    Updated classes human and robot with instance methods.

--- week9/human.py
+++ week9/human.py
@@ -19,3 +19,26 @@ class Human:
     def __str__(self):
         return f"My name is {self.name}, I am {self.age} years old and my energy is {self.energy}"
 
+    # Instant Method
+    def grow(self):
+        self.age += 1
+
+    def eat(self, amount):
+        new_energy = amount + self.energy
+        if new_energy > Human.MAX_ENERGY:
+            self.energy = Human.MAX_ENERGY
+            return new_energy - self.energy
+        else:
+            self.energy = new_energy
+            return 0
+
+    def move(self, distance):
+        new_energy = self.energy - distance
+        if new_energy > 0:
+            self.energy = new_energy
+            return 0
+        else:
+            self.energy = 0
+            return self.energy - abs(new_energy)
+
+
--- week9/robot.py
+++ week9/robot.py
@@ -14,8 +14,29 @@ class Robot:
 
     # Magic Method
     def __repr__(self):
-        return f"Human(name={self.name}, age={self.age}, energy={self.energy})"
+        return f"Robot(name={self.name}, age={self.age}, energy={self.energy})"
 
     def __str__(self):
         return f"My name is {self.name}, I am {self.age} years old and my energy is {self.energy}"
 
+# Instant Method
+    def grow(self):
+        self.age += 1
+
+    def eat(self, amount):
+        new_energy = amount + self.energy
+        if new_energy > Robot.MAX_ENERGY:
+            self.energy = Robot.MAX_ENERGY
+            return new_energy - self.energy
+        else:
+            self.energy = new_energy
+            return 0
+
+    def move(self, distance):
+        new_energy = self.energy - distance
+        if new_energy > 0:
+            self.energy = new_energy
+            return 0
+        else:
+            self.energy = 0
+            return self.energy - abs(new_energy)
