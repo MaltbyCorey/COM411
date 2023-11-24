cd93841fe1c68f6caa2595ff5ab1ed4b8686670d
Corey Maltby <corey.maltby@yahoo.co.uk>
2023-11-24 11:31:22 +0000

    Updated classes human and robot with methods to display objects.

--- week9/human.py
+++ week9/human.py
@@ -12,4 +12,10 @@ class Human:
     def display(self):
         print(f"I am {self.name}")
 
+    # Magic Method
+    def __repr__(self):
+        return f"Human(name={self.name}, age={self.age}, energy={self.energy})"
+
+    def __str__(self):
+        return f"My name is {self.name}, I am {self.age} years old and my energy is {self.energy}"
 
--- week9/robot.py
+++ week9/robot.py
@@ -6,9 +6,16 @@ class Robot:
     def __init__(self):
         self.name = "Robot"
         self.age = 0
-        self.energy = robot.MAX_ENERGY
+        self.energy = Robot.MAX_ENERGY
 
-        # Instant Method
-        def display(self):
-            print(f"I am {self.name}")
+    # Instant Method
+    def display(self):
+        print(f"I am {self.name}")
+
+    # Magic Method
+    def __repr__(self):
+        return f"Human(name={self.name}, age={self.age}, energy={self.energy})"
+
+    def __str__(self):
+        return f"My name is {self.name}, I am {self.age} years old and my energy is {self.energy}"
 
