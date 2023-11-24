c202245e9a5d5fd47ab7cfb3db6ada430fa1352c
Corey Maltby <corey.maltby@yahoo.co.uk>
2023-11-24 11:51:59 +0000

    Created a planet class.

---
+++ week9/planet.py
@@ -0,0 +1,33 @@
+from week9.human import Human
+from week9.robot import Robot
+
+
+class Planet:
+    # Initializer
+    def __init__(self, name):
+        self.name = name
+        self.citizens = {
+            "humans": [],
+            "robots": []
+        }
+
+    # Instance Methods
+    def add_human(self, human):
+        self.citizens["humans"].append(human)
+
+    def remove_human(self, human):
+        self.citizens["humans"].remove(human)
+
+    def add_robot(self, robot):
+        self.citizens["robots"].append(robot)
+
+    def remove_robot(self, robot):
+        self.citizens["robots"].remove(robot)
+
+    # Magic Method
+
+    def __repr__(self):
+        return f"Planet(human={self.citizens['humans']}, Robots(robot={self.citizens['robots']}"
+
+    def __str__(self):
+        return f"The planet has {len(self.citizens['humans'])} humans and {len(self.citizens['robots'])}"
